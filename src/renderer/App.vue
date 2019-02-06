ac660c2bdd05535b059da032e5a3803b7ce77080
Shawn Rakowski <srakowski@live.com>
2019-02-05 19:17:25 -0600

    run scaffolding for electron vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gamecreator',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
