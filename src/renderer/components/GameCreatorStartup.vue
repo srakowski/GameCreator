f3f34ba399350c17d7659360f2fad9b32711e812
Shawn Rakowski <srakowski@live.com>
2019-02-05 18:06:32 -0600

    setup gamecreator scaffolding

---
+++ src/renderer/components/GameCreatorStartup.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Game Creator</h1>
+        <new-game></new-game>
+    </div>
+</template>
+
+<script>
+import NewGame from './NewGame.vue'
+
+export default {
+  components: {
+    NewGame
+  }
+}
+</script>
+
+<style>
+</style>
+
+
---
+++ src/renderer/components/NewGame.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Create something new</h2>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
+
+
