9c75c6f39e2968dfec9890fba5ff7e1c4658473a
Shawn Rakowski <srakowski@live.com>
2019-02-12 19:08:01 -0600

    vue in-depth

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,109 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <list :items="students" @clear="students = []">
+      Hi
+      <div slot="header">
+        <h1>Student List</h1>
+      </div>
+      <div slot="footer">
+        (C) Shawn R
+      </div>
+    </list>
+
+    <list :items="students" @clear="students = []">
+      Hi
+      <div slot="header">
+        <h1>Student List</h1>
+      </div>
+      <div slot="footer">
+        (C) Shawn R
+      </div>
+    </list>
+
+
+    <!-- <span>{{name}}</span>
+    <input v-model="name" type="text" />
+    <button v-on:click="alertMe">TEST</button>
+    <div v-if="name === 'Nicholas'">
+      Nicholas's Stuff
+    </div>
+    <div v-else-if="name === 'Logan'">
+      Logan's Stuff
+    </div>
+    <div v-else>
+      Nobody's Stuff
+    </div>
+    <div v-show="name !== 'Show'">
+      I am shown!
+    </div>
+
+    
+    <!-- <div v-for="(s, index) in students" :key="index">
+      {{s.firstName}}
+      <hr />
+      {{s.lastName}}
+    </div> -->
+
+    <!-- <hr />
+
+    {{fullName}}
+
+    <form @submit.prevent="onSubmit">
+      <label>Name <input type="text" /></label>
+      <button>Submit</button>
+    </form> --> 
+
   </div>
 </template>
 
 <script>
+  import List from './List.vue';
+
   export default {
     name: 'gamecreator',
+    data() {
+      return {
+        name: 'Shawn',
+        students: [
+          { firstName: 'Logan', lastName: 'H' },
+          { firstName: 'Nicholas', lastName: 'R' },
+        ],
+      };
+    },
+    computed: {
+      fullName: {
+        get() {
+          return '';// `${this.students[0].firstName} ${this.students[0].lastName}`;
+        },
+        set(value) {
+          /* eslint-disable */
+          console.log(value);
+        }
+      },
+    },
+    watch: {
+      name(newValue, oldValue) {
+        console.log(oldValue);
+        console.log(newValue);
+      }
+    },
+    methods: {
+      alertMe() {
+        this.name = 'ALERT';
+      },
+      onSubmit(e) {
+        console.error(e);
+      },
+    },
+    created() {
+      // call some api
+    },
+    beforeDestroy() {
+      // check save data
+    },
+    components: {
+      List,
+    },
   };
 </script>
 
---
+++ src/renderer/List.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <slot name="header"></slot>
+      <div v-for="(item, i) in items" :key="i">
+        {{item}}
+      </div>
+      <button @click="clearItems">Clear</button>
+      <slot></slot>
+      <slot name="footer"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    clearItems() {
+      this.$emit('clear');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
